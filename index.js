e26b2f9ed8bd92cc03136a28b7873b6c75e200b4
Rupali97 <rupalidoke97@gmail.com>
2022-05-18 18:08:43 +0530

    full text issue solved

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const dotenv = require("dotenv");
-const nconf = require("nconf");
+import { config } from "dotenv";
+import { get } from "nconf";
 
-dotenv.config();
+config();
 process.env.ROOT_PATH = __dirname;
 
 console.log("here", process.env.NODE_ENV === "production")
-if (nconf.get("NODE_ENV") === "production" || process.env.NODE_ENV === "production") {
+if (get("NODE_ENV") === "production" || process.env.NODE_ENV === "production") {
   require("./dist/index");
 } else {
   require("./src/index");
--- src/bots/twitterMention.ts
+++ src/bots/twitterMention.ts
@@ -1,6 +1,6 @@
-// import Twit from 'twit'
+import Twit from 'twit'
 // import Twitter from 'twitter-v2'
-import {ETwitterStreamEvent, TwitterApi} from 'twitter-api-v2';
+import { TwitterApi} from 'twitter-api-v2';
 
 import nconf from 'nconf'
 
@@ -11,7 +11,7 @@ import {config} from '../utils/config'
 export const twitterMetions = async(mode: any) => {
 
   console.log("here");
-  
+
   const trackWords = [
     '$MAHA',
     '@TheMahaDAO',
@@ -21,12 +21,19 @@ export const twitterMetions = async(mode: any) => {
   let msgTemplate: string
   const discord_channel = config().staging.DISCORD
 
-  const clientv1 = new TwitterApi({
-      appKey: nconf.get("TWITTER_API_KEY"),
-      appSecret: nconf.get("TWITTER_API_SECRET"),
-      accessToken: nconf.get("TWITTER_ACCESS_TOKEN"),
-      accessSecret: nconf.get("TWITTER_ACCESS_TOKEN_SECRET")
-  }).v1
+  // const clientv1 = new TwitterApi({
+  //     appKey: nconf.get("TWITTER_API_KEY"),
+  //     appSecret: nconf.get("TWITTER_API_SECRET"),
+  //     accessToken: nconf.get("TWITTER_ACCESS_TOKEN"),
+  //     accessSecret: nconf.get("TWITTER_ACCESS_TOKEN_SECRET")
+  // }).v1
+
+  const clientv1 = new Twit({
+    consumer_key: nconf.get("TWITTER_API_KEY"),
+    consumer_secret: nconf.get("TWITTER_API_SECRET"),
+    access_token: nconf.get("TWITTER_ACCESS_TOKEN"),
+    access_token_secret: nconf.get("TWITTER_ACCESS_TOKEN_SECRET")
+   })
 
   const clientv2 = new TwitterApi(nconf.get('BEARER_TOKEN')).v2
 
@@ -38,26 +45,41 @@ export const twitterMetions = async(mode: any) => {
 
   console.log('whiteListedUsers', whiteListedUsers)
 
- const streamFilter = await clientv1.filterStream(
-    { track: trackWords,
-      follow: whiteListedUsers,
-      // tweet_mode: 'extended'
+//  const streamFilter = await clientv1.filterStream(
+//     { track: trackWords,
+//       follow: whiteListedUsers,
+//       // tweet_mode: 'extended'
 
-    });
+//     });
+
+  const streamFilter = clientv1.stream('statuses/filter', {
+    follow: whiteListedUsers,
+    track: trackWords,
+    tweet_mode: 'full_text'
+  })
 
 //  eslint-disable-next-line @typescript-eslint/no-explicit-any
    streamFilter
-    .on(ETwitterStreamEvent.Data, (tweet: any) => {
+    .on('tweet', (tweet: any) => {
 
       if(whiteListedUsers.includes(tweet.user.id_str) &&
-        (trackWords.some(word => tweet.text.includes(word))) &&
+        (trackWords.some(word =>
+          {
+            if(tweet.extended_tweet){
+              return tweet.extended_tweet.full_text.includes(word)
+            }else{
+              return tweet.text.includes(word)
+            }
+          }
+        )
+          ) &&
           !tweet.retweeted && !tweet.retweeted_status && !tweet.in_reply_to_status_id &&
           !tweet.in_reply_to_status_id_str && !tweet.in_reply_to_user_id &&
           !tweet.in_reply_to_user_id_str && !tweet.in_reply_to_screen_name
           ) {
           console.log('if tweet', tweet)
           console.log('discord_channel', discord_channel)
-          msgTemplate = `${tweet.text}`
+          msgTemplate = tweet.extended_tweet ? tweet.extended_tweet.full_text : tweet.text
 
           discord.sendMessage(
             mode === 'production' ? config().production.DISCORD.twitterMention : config().staging.DISCORD,
